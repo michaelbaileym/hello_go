7a1c138652431bc2b852a95d4c5a473f90a7d723
michaelbaileym <michael.baileym@gmail.com>
2018-05-17 21:01:56 -0700

    idiomatic variables lesson

---
+++ idiomatic_variables.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+)
+
+var s = "Hello World!"
+
+func main()  {
+  i := 42
+  fmt.Println(s)
+  fmt.Println(i)
+}
+
+//Ways to declare variables in Go
+// var x = "Hello World"
+// var x string = "Hello World"
+// var t string
+// t = "hello world"
+// u:= "hello world"  can only be used inside a function
