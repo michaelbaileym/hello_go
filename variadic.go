21c461f592c609fd3cb26dda770f3634864e49a2
michaelbaileym <michael.baileym@gmail.com>
2018-05-19 00:17:41 -0700

    added variadic

---
+++ variadic.go
@@ -0,0 +1,17 @@
+//Variadic functions are functions that accept a variable number of arguments
+package main
+
+import "fmt"
+
+func sumNumbers(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func main() {
+	result := sumNumbers(1, 2, 3, 4)
+	fmt.Printf("The result is %v\n", result)
+}
