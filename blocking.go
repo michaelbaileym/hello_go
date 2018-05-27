f2ecba12d7f791d33161902b1c898f6bfdb07a1d
michaelbaileym <michael.baileym@gmail.com>
2018-05-27 13:41:16 -0700

    latency calls

--- blocking.go
+++ blocking.go
@@ -2,19 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
 
+/*
 func slowFunc() {
 	time.Sleep(time.Second * 2)
 	fmt.Println("sleeper() finished")
 
+} */
+
+func responseTime(url string) {
+	start := time.Now()
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	elapsed := time.Since(start).Seconds()
+
+	fmt.Printf("%s took %v seconds \n", url, elapsed)
 }
 
 func main() {
-	go slowFunc()
+
+	urls := make([]string, 3)
+	urls[0] = "https://www.usa.gov/"
+	urls[1] = "https://www.gov.uk/"
+	urls[2] = "http://www.gouvernement.fr/"
+
+	for _, u := range urls {
+		responseTime(u)
+	}
+
+	/*go slowFunc()
 	fmt.Println("I am not shown until slowFunc() completes")
 	time.Sleep(time.Second * 3)
-
-	//fmt.Println("I am shown right away")
+	fmt.Println("I am shown right away") */
 }
